c70c360e4a4d07f2f47311eaa93eeca304f1056d
WaridIIUC <warid.cse.iiuc@gmail.com>
2021-02-10 13:14:33 +0600

    git

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ const displaySongs = songs => {
                 <h3 class="lyrics-name">${song.title}</h3>
                 <p class="author lead">Album by <span>${song.artist.name}</span></p>
                 <audio controls>
-                    <source src="${
-                        git c
-                    }" type="audio/ogg">
+                    <source src="${song.preview}" type="audio/ogg">
                 </audio>
                 </div>
             <div class="col-md-3 text-md-right text-center">
